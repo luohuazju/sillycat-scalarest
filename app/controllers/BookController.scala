ccdfbf90cef8ba9ca9ad35d2efd7a90edc3de911
Carl Luo <cluo@jobs2careers.com>
2015-09-08 10:59:10 -0500

    simple example, not production level

--- app/controllers/BookController.scala
+++ app/controllers/BookController.scala
@@ -2,12 +2,12 @@ package controllers
 
 import play.api.libs.json._
 import play.api.mvc._
-import models.Book._
+import models.Book
 
 object BookController extends Controller {
 
   def listBooks = Action {
-    Ok(Json.toJson(books))
+    Ok(Json.toJson(Book.books))
   }
 
   def saveBook = Action(BodyParsers.parse.json) { request =>
@@ -17,9 +17,36 @@ object BookController extends Controller {
         BadRequest(Json.obj("status" -> "OK", "message" -> JsError.toFlatJson(errors)))
       },
       book => {
-        addBook(book)
+        Book.addBook(book)
         Ok(Json.obj("status" -> "OK"))
       }
     )
   }
+
+  def getBook(id:String) = Action {
+    Book.getBook(id) match {
+      case Some(b) => Ok(Json.toJson(b))
+      case _ => Ok(Json.obj("status" -> "OK", "message" -> "Book Not Found!"))
+    }
+  }
+
+  def deleteBook(id:String) = Action {
+    Book.deleteBook(id)
+    Ok(Json.obj("status" -> "OK"))
+  }
+
+  def updateBook(id:String) = Action(BodyParsers.parse.json){ request =>
+    val b = request.body.validate[Book]
+    b.fold(
+      errors => {
+        BadRequest(Json.obj("status" -> "OK", "message" -> JsError.toFlatJson(errors)))
+      },
+      book => {
+        book.id = Some(id)
+        Book.updateBook(book)
+        Ok(Json.obj("status" -> "OK"))
+      }
+    )
+  }
+
 }
--- app/models/Book.scala
+++ app/models/Book.scala
@@ -2,14 +2,45 @@ package models
 
 import play.api.libs.json.Json
 
-object Book {
+case class Book(var id:Option[String], title: String, author: String, isbn: String)
 
-  case class Book(name: String, author: String)
+object Book {
 
   implicit val bookWrites = Json.writes[Book]
   implicit val bookReads = Json.reads[Book]
 
-  var books = List(Book("TAOCP", "Knuth"), Book("SICP", "Sussman, Abelson"))
+  var books = List(
+    Book(Some("1"),"Python in Action", "Xman", "isbn-001"),
+    Book(Some("2"), "R in Action", "Hero", "isbn-002")
+  )
 
-  def addBook(b: Book) = books = books ::: List(b)
+  def addBook(b: Book) = {
+    val r = scala.util.Random
+    b.id = Some(r.nextInt(10000000).toString)
+    books = books ::: List(b)
+  }
+
+  def getBook(id:String):Option[Book] = {
+    val finds = books filter { book =>
+      book.id.get.equals(id)
+    }
+    finds.size match {
+      case 1 => Some(finds(0))
+      case _ => None
+    }
+  }
+
+  def deleteBook(id:String) = {
+    val filters = books filterNot { book =>
+      book.id.get.equals(id)
+    }
+    books = filters
+  }
+
+  def updateBook(b:Book) = {
+    val filters = books filterNot { book =>
+      book.id.get.equals(b.id.get)
+    }
+    books = filters ::: List(b)
+  }
 }
